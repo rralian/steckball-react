43c5b18e9a9b02a784141d6fb15c26638c4a7444
Bob Ralian <bob.ralian@gmail.com>
2015-11-29 22:35:36 -0600

    user data is reactive now

--- components/app.jsx
+++ components/app.jsx
@@ -1,7 +1,16 @@
 // App component - represents the whole app
 App = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+	return {
+		currentUser: Meteor.user()
+	};
+  },
 
   render() {
+	if ( ! this.data.currentUser ) {
+		return <p>Register or login to get started.</p>;
+	}
     return (
       <div className="container">
         <header>
