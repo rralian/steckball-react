c7e620601210fef311ca96cf2af8a0c48f1dbfcf
Bob Ralian <bob.ralian@gmail.com>
2015-12-13 12:04:50 -0600

    improve homepage message for logged out user

--- components/home.jsx
+++ components/home.jsx
@@ -5,16 +5,30 @@ Home = React.createClass({
         return {
         	picks: Picks.find().fetch(),
             myPicks: Picks.find({userId: Meteor.userId()}).fetch(),
+            currentUser: Meteor.user(),
         };
     },
+    loginRegister( event ) {
+        event.preventDefault();
+        Accounts._loginButtonsSession.set('dropdownVisible', true);
+    },
     render() {
-        const { picks, myPicks } = this.data;
+        const { picks, myPicks, currentUser } = this.data;
+        const callToAction = currentUser ?
+            <p>Looks like you haven't created any picks yet. Why not <Link to="/picks/new">create your picks</Link> and join in?</p>
+            :
+            <p>Looks like you aren't signed in. First thing's first, <a href="/login" onClick={ this.loginRegister }>login/register</a> to get started.</p>
+        ;
         return (
             <div className="home">
                 { ! myPicks.length &&
-                <p>Welcome! SteckBall is a fun little family bet to see who can
-                predict the NCAA bowl game results the best. Looks like you haven't
-                created any picks yet. Why not <Link to="/picks/new">create your picks</Link> and join in?</p> }
+                <div>
+                    <p>
+                        Welcome! SteckBall is a fun little family bet to see who can
+                        predict the NCAA bowl game results the best.
+                    </p>
+                    { callToAction }
+                </div> }
                 <h1>Standings</h1>
                 <p>Here are the picks and standings so far.</p>
                 <ul className="all-picks">
