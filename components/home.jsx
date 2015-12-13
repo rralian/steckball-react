4a76e3309a71fbe193ca8a5e0c47b58db08e191c
Bob Ralian <bob.ralian@gmail.com>
2015-12-13 09:38:15 -0600

    couple of little tweaks to the homepage

--- components/home.jsx
+++ components/home.jsx
@@ -11,12 +11,12 @@ Home = React.createClass({
         const { picks, myPicks } = this.data;
         return (
             <div className="home">
-                <h1>Home</h1>
                 { ! myPicks.length &&
                 <p>Welcome! SteckBall is a fun little family bet to see who can
                 predict the NCAA bowl game results the best. Looks like you haven't
                 created any picks yet. Why not <Link to="/picks/new">create your picks</Link> and join in?</p> }
-                <h2>All Picks</h2>
+                <h1>Standings</h1>
+                <p>Here are the picks and standings so far.</p>
                 <ul className="all-picks">
                     { picks.map( pick =>
                         <li key={ `pick-${pick._id}` }>{ pick.totalScore } -- { pick[ 'pick-owner' ] }</li>
